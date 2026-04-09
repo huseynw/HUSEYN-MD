e651b1518570f547d95adedef611209f0fb8932c
huseynw <huseynmemmedov9272@gmail.com>
2026-04-09 19:35:06 +0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+import "dotenv/config";
+
+export const BOT_NAME = "HUSEYN-MD";
+export const SESSION_PREFIX = "HUSEYN-MD";
+export const AUTH_DIR = "./auth";
+export const COMMAND_PREFIX = ".";
+
+export const OWNER_NUMBER = process.env.OWNER_NUMBER || "";
+export const SESSION_ID = process.env.SESSION_ID || "";
+export const SESSION_PASSWORD = process.env.SESSION_PASSWORD || "";
